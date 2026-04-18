3f3155ce2cd653669e5144781aeb77413b700535
Shivam Mehto <smetro65@gmail.com>
2026-04-19 02:06:29 +0530

    feat: moved the upload function in upload.py

---
+++ src/bronze/ingestion/upload.py
@@ -0,0 +1,6 @@
+# SparkSession is provided by Databricks runtime
+# For local execution, initialize manually
+from coin_market import *
+
+upload_coin_market_top_by_market_cap()
+upload_coin_market_top_by_volume()  
