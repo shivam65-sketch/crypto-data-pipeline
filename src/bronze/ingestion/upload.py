fb0ce91a7d425034d0f636d0670878866fc363bb
Shivam Mehto <smetro65@gmail.com>
2026-04-19 03:58:52 +0530

    Revert "feat: added exchanges upload"
    This reverts commit 0e0fe1767e4029bb153f45c4c4c07a340dba514c.

--- src/bronze/ingestion/upload.py
+++ src/bronze/ingestion/upload.py
@@ -1,11 +1,9 @@
 from pyspark.sql import SparkSession
 from coin_market import upload_coin_market_top_by_market_cap, upload_coin_market_top_by_volume
 from coin_list import upload_coins_list 
-from exchanges import upload_exchanges_list
 
 spark = SparkSession.builder.appName("Crypto").getOrCreate()
 
 upload_coin_market_top_by_market_cap(spark)
 upload_coin_market_top_by_volume(spark)  
 upload_coins_list(spark)
-upload_exchanges_list(spark)
